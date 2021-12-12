ec390d289319f3794fd36d54ed2a5208f367ed3e
Maycon Santos <mayconalves.santos14@gmail.com>
2021-12-12 15:04:13 -0300

    add config

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,16 @@ import Phaser from 'phaser';
 export default {
   type: Phaser.AUTO,
   parent: 'game',
-  backgroundColor: '#33A5E7',
+  transparent: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 }
+    }
+  },
   scale: {
     width: 800,
-    height: 600,
+    height: 400,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   }
